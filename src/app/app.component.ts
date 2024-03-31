b436d13f1d41b4a66da2ba7adea065f642763915
Mike1817 <mike@hohenschlaeger.com>
2024-03-31 15:30:07 +0200

    final push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MessengerComponent } from './messenger/messenger.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MessengerComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
